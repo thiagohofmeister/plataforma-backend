141deab0f2b473165435537726ec85275a384a84
Thiago Hofmeister <thiago.hofmeister@gmail.com>
2018-01-22 18:53:45 -0200

    Adiciona encoder de utf8

---
+++ src/Encoder/Exception/Utf8Exception.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Encoder\Exception;
+
+use Exception;
+
+/**
+ * Tratamento de exceções lançadas durante a codificação de Utf8.
+ *
+ * @author Thiago Hofmeister <thiago.souza@moovin.com.br>
+ */
+class Utf8Exception extends Exception
+{
+
+}
